2bca225d8751005efd00fdb9947961f164b32f73
Scottacious <scottgeilen99@gmail.com>
2019-06-28 16:48:55 -0400

    removed bugs

--- AgeCalculator01.cpp
+++ AgeCalculator01.cpp
@@ -57,35 +57,3 @@ int main() {
         break;
     }
 }
-=======
-using namespace std;
-
-int main() {
-    int current_year, born, age_year;
-    string name;
-    char birthday;
-
-    cout << "\nWhat is your name? ";
-    cin >> name;
-    cout << "\nWhat year is it currently? ";
-    cin >> current_year;
-    cout << "\nWhat year were you born? ";
-    cin >> born;
-
-    age_year = current_year - born;
-    cout << "\nDid you already have your birthday this year, " << name << "? (y/n) ";
-    cin >> birthday;
-    
-    switch(birthday)
-    {
-        case 'y':
-            cout << "\nYou are " << age_year << " years old. \n\nHappy belated birthday, " << name << "!\n\n";
-            break;
-        case 'n':
-            cout << "\nYou will turn " << age_year << " years old this year. \n\nHappy early birthday, " << name << "!\n\n";
-            break;
-        default:
-            cout << "\nSorry, what? \n\nExiting program because " << name << " can't follow simple directions. \nSmh humans...\n\n";
-    }
-}
->>>>>>> 4d0275316c8325166b60f534ee88fa84e2913381
